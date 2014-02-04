073ac60ed9d8b7829be63168a8f24e0065e0c0e0
jesse keane <me@jessekeane.me>
2014-02-04 13:45:49 -0800

    add save@ support

--- README.md
+++ README.md
@@ -14,31 +14,31 @@ etc.
 
 ## how to use it
 
-simply add a key to your `package.json` like so:
+save dependencies to your `package.json` automatically using
+`npmm install --save@http://myprivate.npm secret-module`
+
+or manually add a key to your `package.json` like so:
 
 ```js
 {
-  "dependencies@http://myawesome.npm": {
-    "private-module": "~1.2.3"
+  "dependencies@http://myprivate.npm": {
+    "secret-module": "~1.2.3"
   }
 }
 ```
 
 ...and then...
 
-`npmm i`
+`npmm install`
 
 anything in the standard `dependencies` object will be installed from the
-default registry (or the one designated in your `.npmrc` if applicable)
+default registry (or the one designated in your `.npmrc` if applicable) and
+any packages in a `dependencies@npm-registry-location` key will be installed
+from `npm-registry-location`.
 
 ## notes
 
 any other commands you throw at npmm will just get proxied to the real npm
-
-## wip
-
-* make it easy to add dependencies to `package.json`
-(`npmm i --save@http://my.npm modulename`)
 
 ## todo
 
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var spawn = require('child_process').spawn,
     color = require('bash-color'),
+    split = require('split'),
+    write_package = require('./lib/write-package'),
     path = require('path')
 
 module.exports = npmm
@@ -7,6 +9,8 @@ module.exports = npmm
 function npmm(_args) {
   var args = (_args || []).slice(2),
       is_npmm_save = /(--save|-S)@(.*?)$/,
+      packages = [],
+      save_command = 0,
       to_registry = null
 
   if (args.indexOf('install') === -1 && args.indexOf('i') === -1 &&
@@ -17,13 +21,24 @@ function npmm(_args) {
   if (args.length === 1) return filter_package()
 
   for (var i = 0, l = args.length; i < l; ++i) {
-    if (is_npmm_save.test(args[i])) {
-      to_registry = args[i].match(is_npmm_save)[2]
+    to_registry = args[i].match(is_npmm_save)
+    if (to_registry) {
+      save_command = i
+      to_registry = to_registry[2]
       break
     }
   }
 
   if (!to_registry) return exec_npm(args)
+  args.splice(i, 1)
+  packages = args.slice(0)
+
+  args = args.concat(['--save', '--registry', to_registry])
+  exec_npm(args, do_write)
+
+  function do_write() {
+    write_package(to_registry, packages)
+  }
 
   function filter_package() {
     var package = require(path.join(process.cwd(), 'package.json')),
@@ -82,6 +97,8 @@ function npmm(_args) {
 
     npm = spawn('npm', npm_args, { stdio: 'inherit' })
     npm.on('close', cb)
+
+    return npm
   }
 }
 
